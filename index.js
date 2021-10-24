c9063dc81e5e3c5fe4700974309ad08ad8a817af
Артем Казанцев <akibog@mail.ru>
2021-10-24 19:08:31 +0700

    added gen

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const test = require('./lib/index');
+#!/usr/bin/env node
+
 const yargs = require('yargs');
+const path = require('path');
+
+const getConfig = require('./lib/get-config');
+const gen = require('./lib/gen');
+
+const config = getConfig(path.join(__dirname, '..'));
 
 yargs.command(
   'hello',
@@ -11,5 +18,31 @@ yargs.command(
   }
  },
  argv => { console.log(argv) }
+).help()
+.argv;
+
+
+yargs.command(
+  'gen',
+  'Generate',
+ {
+  type: {
+    alias: 't',
+    default: 'component',
+  },
+  name: {
+    alias: 'n',
+  },
+  'style-import-name': {
+    alias: 'sin'
+  }
+ },
+ argv => { 
+   try {
+     gen(config, argv.type, argv.name, argv['style-import-name'])
+   } catch (err) {
+     console.log(err);
+   }
+ }
 ).help()
 .argv;
